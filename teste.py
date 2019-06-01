6307aedc11e5b5504a73bcbfe1174a9d46d0121f
ppcamp <p.augustocampos@gmail.com>
2019-06-01 17:51:59 -0300

    clean

--- teste.py
+++ teste.py
@@ -5,7 +5,7 @@ from packages.sigaa.history import HistoryScraping
 paa = HistoryScraping()
 
 # Login with RA and psswd
-paa.login(user='12638891665', password='Sigaa*0223')
+paa.login(user='RA', password='SENHA')
 
 # DO fluxogram
 paa.toDiagram()
