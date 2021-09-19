dcd737c9e2817d5c09d22f846f6aadf1acf8f380
ppcamp <p.augustocampos@gmail.com>
2021-09-19 14:38:12 -0300

    feat: add hardware and software students

--- src/modules/Score.py
+++ src/modules/Score.py
@@ -28,6 +28,9 @@ import tabula
 from pandas import DataFrame
 # Parse json to string
 import json
+# numpy object
+import numpy as np
+import random
 
 
 def read_json(filepath: str) -> json:
@@ -248,3 +251,111 @@ def parse_pdf(studentId: str, inputDir: str, outDir: str = None) -> DataFrame:
 
     # Return the json object
     return out
+
+
+
+def gen_score(
+  rounded: int = 2,
+  start: float = 7.0,
+  end: float = 10.0) -> float:
+  """
+  Generates a random float number between `start` and `end` rounded by `rounded`
+
+  Kwargs
+  ----
+  `rounded`:
+    The number of decimal places
+  `start`:
+    The first number that can be generated
+  `end`:
+    The last number that can be generated
+  """
+  return round(random.uniform(start,end),rounded)
+
+
+"""DISCIPLINES is the list of classes splitted by its focused content"""
+DISCIPLINES = {
+  "HARDWARE": np.array([
+    "EELI02",
+    "EELI03",
+    "ECOI12",
+    "EELI07",
+    "ECAI26",
+    "EELI10",
+    "EELI11",
+    "ECOI10",
+    "EELI12",
+    "EELI13",
+    "EELI14",
+    "EELI15",
+    "ECAI04",
+    "ECOI21",
+    "ECAI11",
+    "ECAI44",
+    "ECOI18",
+    "ECOI33",
+    "ECAI05",
+    "ECAI07",
+    "ECOI33",
+    "ECAI13",
+    "ECOI07",
+    "ECOI32",
+    "ECOI19",
+  ]),
+  "SOFTWARE": np.array([
+    "ECOI02",
+    "ECOI03",
+    "ECOI04",
+    "ECOI08",
+    "ECOI14",
+    "ECOI30",
+    "ECOI15",
+    "ECOI16",
+    "ECOI11",
+    "ECOI13",
+    "ECOI22",
+    "ECOI25",
+    "ECOI09",
+    "ECOI23",
+    "ECOI24",
+    "ECOI26",
+  ]),
+  "UNCLASSIFIED": np.array([
+    'EAMI30',
+    'ECAI29',
+    'ECO038',
+    'ECOI01',
+    'ECOI20',
+    'ECOI61',
+    'EMBI02',
+    'EMEI02',
+    'EMEI06',
+    'EMEI07',
+    'EMEI08',
+    'EMTI02',
+    'EMTI03',
+    'EPRI02',
+    'EPRI04',
+    'EPRI30',
+    'FISI01',
+    'FISI02',
+    'FISI03',
+    'FISI04',
+    'FISI04',
+    'FISI05',
+    'FISI06',
+    'FISI07',
+    'HUMI01',
+    'HUMI02',
+    'HUMI04',
+    'HUMI06',
+    'MATI01',
+    'MATI02',
+    'MATI03',
+    'MATI03',
+    'MATI04',
+    'MATI05',
+    'MATI06',
+    'MATI07',
+    'MATI08'])
+}
